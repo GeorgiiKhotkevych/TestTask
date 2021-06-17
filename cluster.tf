c9fea221d82508a2285ff0f68d00022c5feb7286
George Khotkevych <georgiikhotkevych@gmail.com>
2021-06-17 15:24:19 +0300

    Update cluster.tf

--- cluster.tf
+++ cluster.tf
@@ -1,13 +1,20 @@
-provider "aws" {
-	access_key = "AKIAVD3TZXDYLINAVMNN"
-	secret_key = "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"
+#----------------------------------------------------------
+# Terraform
+#
+# k8s cluster on AWS with EFK
+#
+# Made by georgii
+#----------------------------------------------------------provider "aws" {
+	access_key = ""
+	secret_key = ""
 	region = "eu-central-1"
 }
 
-resource "aws_instance" "MyUbuntu" {
-	ami = "ami-05f7491af5eef733a"
+resource "aws_instance" "Ubuntu" {
+	ami = "ami-05f7491af5eef733a" #Ubuntu Server 20.04 LTS (HVM), SSD Volume Type
 	instance_type = "t2.micro"
 	count = 4
+	user_data = <<EOF
 
 	tags = {
 	Name = "My_TF_Cluster"
